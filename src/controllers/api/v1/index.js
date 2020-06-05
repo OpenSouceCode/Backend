a825eea3d7b33643e199c72ac58369747df0f377
ShubhamxKumar <55661658+ShubhamxKumar@users.noreply.github.com>
2020-06-05 21:13:53 +0530

    issue-5

--- src/controllers/api/v1/index.js
+++
@@ -1,7 +0,0 @@
-const create = require('../../create');
-
-module.exports = {
-  getStatus: create((req, res) => {
-    res.status(200).json({ status: 'Running' });
-  }),
-};
--- src/routes/api/v1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const controller = require('../../../controllers/api/v1/index');
-
-router.get('/status', controller.getStatus);
-
-module.exports = router;
