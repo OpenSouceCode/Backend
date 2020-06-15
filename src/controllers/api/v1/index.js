56942a46bede69f9a1f2b213e2768c142a2916db
Satyam <rainasatyam34@gmail.com>
2020-06-15 19:50:57 +0530

    issue 1

--- src/controllers/api/v1/index.js
+++ src/controllers/api/v1/index.js
@@ -1,7 +1,15 @@
 const create = require('../../create');
 
 module.exports = {
-  getStatus: create((req, res) => {
-    res.status(200).json({ status: 'Running' });
-  }),
+    url: "https://api.github.com/users/username/repos",
+    jsonp: true,
+    method: "GET",
+    dataType: "json",
+    success: function(res) {
+        console.log(res)
+    },
+    getStatus: create((req, res) => {
+        res.status(200).json({ status: 'Running' });
+    }),
+
 };
