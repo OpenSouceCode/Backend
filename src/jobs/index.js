695ea19ea4b4770974ad5a199585d588096a61b1
Irshad Ansari <irshadjsr21@gmail.com>
2020-07-18 22:47:19 +0530

    [Feat] Added fetching issue and PR with repo

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,8 +1,5 @@
 const { langRepoJob } = require('./github/langRepo');
 const { orgRepoJob } = require('./github/orgRepo');
-const PRRepoJob = require('./github/prRepo');
 
 langRepoJob.start();
 orgRepoJob.start();
-/*  Start storing the pull requests */
-PRRepoJob.start();
