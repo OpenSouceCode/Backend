998d22b6bc22b42fd43af0258efd7a2bae75fd17
Dishebh Bhayana <dishebh27@gmail.com>
2020-08-23 17:35:33 +0530

    added route to list all PRs

--- src/routes/v1/github.js
+++ src/routes/v1/github.js
@@ -6,5 +6,6 @@ const authenticator = require('../../middlewares/auth');
 const controller = require('../../controllers/v1/github');
 
 router.get('/repositories', authenticator, controller.getRepos);
+router.get('/pulls/:owner/:repos', authenticator, controller.getPullRequests);
 
 module.exports = router;
