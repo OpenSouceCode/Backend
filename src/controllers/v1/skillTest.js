bca1f84d24fb7c165d9a5b6b252bb7d4bafdad7a
Dishebh Bhayana <dishebh27@gmail.com>
2020-09-18 22:48:33 +0530

    added route to add a skill test

---
+++ src/controllers/v1/skillTest.js
@@ -0,0 +1,28 @@
+const create = require('../create');
+// eslint-disable-next-line import/no-unresolved
+const SkillTest = require('../../models/SkillTest');
+const validators = require('../../validators/skillTest');
+
+module.exports = {
+  postSkillTest: create(
+    async (req, res) => {
+      const { name, image, description } = req.body;
+
+      const newSkillTest = new SkillTest({
+        name,
+        image,
+        description,
+      });
+
+      const skillTest = await newSkillTest.save();
+
+      res.json({ data: skillTest });
+    },
+    {
+      validation: {
+        validators: validators.postSkillTest,
+        throwError: true,
+      },
+    },
+  ),
+};
