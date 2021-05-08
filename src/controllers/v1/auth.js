3ddceb93ec2a7d330eef09dce737606287c62293
aryasoni98 <aryasoni98@gmail.com>
2021-05-08 22:41:17 +0530

    Bug
    Issue with data requested using github-oauth

--- src/controllers/v1/auth.js
+++ src/controllers/v1/auth.js
@@ -8,7 +8,6 @@ module.exports = {
     const { redirectTo } = req.query;
     const state = JSON.stringify({ redirectTo });
     const authenticator = passport.authenticate('github', {
-      scope: ['read:user', 'user:email', 'repo'],
       state,
       session: true,
     });
